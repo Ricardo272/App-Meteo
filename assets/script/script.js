a31944c9ba25685206e9194bcbcc12b1a1c83bed
Ricardo <ricardo.remond.pro@gmail.com>
2023-11-04 19:19:37 +0100

    commit

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,37 @@
 let city = "rouen"
 const myAPIKey = "1ed69be7f97ce4c145cec77bf1e9bb23"
 
+
+document.getElementById("rechercher").addEventListener("click", function () {
+
+
+    document.querySelector(".weatherData").innerHTML = ""
+
+
+    let city = document.querySelector("#search").value
+
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${myAPIKey}&units=metric&lang=fr`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+
+            let count = 1
+            for (let day of data.list) {
+                if (count == 6) {
+                    return
+
+                }
+            }
+
+        })
+})
+
+
+
+
+
+
+
 fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${myAPIKey}&units=metric&lang=fr`)
     .then(response => response.json())
     .then(data => {
